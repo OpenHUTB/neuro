6e9513306dffea27ea6d0cbc9625f6e36a41aeaf
王海东 <wanghaidong@hutb.edu.cn>
2025-02-27 16:55:35 +0800

    根据书签获取切分点 (#393)
    * 根据书签获取切分点
    
    * fix
    
    ---------
    
    Co-authored-by: nongfugengxia <nongfugengxia@163.com>

--- src/split_pdf_with_index.py
+++ src/split_pdf_with_index.py
@@ -6,6 +6,8 @@ import numpy as np
 
 import PyPDF2
 
+import fitz  # 导入pymupdf库
+
 
 def read_pdf(file_path):
     with open(file_path, 'rb') as file:
@@ -47,82 +49,34 @@ def save_pdf(pdf_writer, output_path):
 if __name__ == '__main__':
     file_path = '../neuro.pdf'
 
-    section_info = np.array([
-        [1,  -1],   # 0
-        [30, -1],  # 1
-        [48, -1],  # 2
-        [74, 117],  # 3
-        [90, 141],  # 4
-        [113, 155],  # 5
-        [125, 172],  # 6
-        [138, 209],  # 7
-        [170, 234],  # 8
-        [190, 255],  # 9
-        [211, 280],  # 10
-        [233, 298],  # 11
-        [247, 317],  # 12
-        [265, 345],  # 13
-        [293, 368],  # 14
-        [314, 402],  # 15
-        [342, 423],  # 16
-        [358, 452],  # 17
-        [380, 479],  # 18
-        [405, 514],  # 19
-        [436, 540],  # 20
-        [462, 565],  # 21
-        [484, 589],  # 22
-        [507, 608],  # 23
-        [527, 626],  # 24
-        [543, 642],  # 25
-        [558, 673],  # 26
-        [587, 695],  # 27
-        [608, 726],  # 28
-        [634, 751],  # 29
-        [656, 781],  # 30
-        [681, 805],  # 31
-        [703, 827],  # 32
-        [723, 859],  # 33
-        [750, 904],  # 34
-        [791, 927],  # 35
-        [813, 952],  # 36
-        [837, 976],  # 37
-        [858, 997],  # 38
-        [876, 1018],  # 39
-        [894, 1054],  # 40
-        [923, 1089],  # 41
-        [954, 1109],  # 42
-        [970, 1124],  # 43
-        [983, 1144],  # 44
-        [1001, 1174],  # 45
-        [1028, 1200],  # 46
-        [1053, 1225],  # 47
-        [1076, 1254],  # 48
-        [1103, 1280],  # 49
-        [1128, 1304],  # 50
-        [1152, 1329],  # 51
-        [1173, 1356],  # 52
-        [1194, 1383],  # 53
-        [1217, 1414],  # 54
-        [1244, 1436],  # 55
-        [1262, 1461],  # 56
-        [1284, 1491],  # 57
-        [1309, 1517],  # 58
-        [1333, 1532],  # 59
-        [1347, 1545],  # 60
-        [1359, 1567],  # 61
-        [1376, 1588],  # 62
-        [1392, -1],  # 63
-        [1406, -1],  # 64
-        [1426, -1],  # 中英文对照表
-        [1500, -1],  # 参考文献
-        [1528+1, -1],  # 用于控制结束
-    ])
-
     # pdf_reader = read_pdf(file_path)
     # print(pdf_reader.pages)
     output_dir = "../docs/pdf"
 
-    # seek of closed file
+    # 根据书签获取切分点
+    doc = fitz.open(file_path)
+    toc = doc.get_toc()
+    section_info = np.empty((64 + 2, 2), dtype=int)
+    section_info[0, 0] = 1
+    split_num = 1
+    is_first = False
+    for i in range(len(toc)):
+        indent_level, bookmark_name, redirect_page = toc[i]  # [缩进级别, 书签名, 书签跳转的页码]
+        if indent_level <= 2:
+            if indent_level == 1:  # 如果是某一部分，则这部分的第一章不作为切分点，而是这一部分的开始作为切分点
+                section_info[split_num, 0] = redirect_page
+                split_num = split_num + 1
+                is_first = True
+            else:
+                if is_first == True:
+                    is_first = False
+                    continue
+                else:
+                    section_info[split_num, 0] = redirect_page
+                    split_num = split_num + 1
+    section_info[65, 0] = doc.page_count + 1  # 用于控制结束
+
+    # 切分PDF
     # 一旦离开with块，该文件句柄就会关闭，所以不能用read_file()函数
     with open(file_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
